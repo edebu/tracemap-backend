d661d25994722b8c3a34a15dad2a18f75cba4c0f
Burak Ede <burak.ede24@gmail.com>
2025-04-09 13:57:22 +0300

    fix(api/index.js): remove redundant traceroute call in exec command to prevent unnecessary execution ci(package.json): add verification of traceroute installation path in vercel-build script to ensure dependency availability

--- api/index.js
+++ api/index.js
@@ -32,7 +32,7 @@ app.get("/api/traceroute", (req, res) => {
     //     }
     // }
 
-    exec(`traceroute && traceroute ${target}`, (err, stdout, stderr) => {
+    exec(`traceroute ${target}`, (err, stdout, stderr) => {
         if (err) {
             return res.status(500).json({ error: stderr });
         }
